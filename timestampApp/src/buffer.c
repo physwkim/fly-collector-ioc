e9c72a54b9f215d5e81cb1096ba4dc967525332e
Stuart B. Wilkins <swilkins@bnl.gov>
2015-08-02 11:24:25 -0400

    [ENH] Added switch to erasing buffer

--- timestampApp/src/buffer.c
+++ timestampApp/src/buffer.c
@@ -14,7 +14,9 @@ static long buffer(aSubRecord *prec) {
   /* 
    * Field A is the switch to start recording. 
    * Field B is the input field. 
-   * Field C is the output (waveform)
+   * Field VALA is the output (waveform)
+   * Field VALB is the number of elements in the buffer
+   * Field VALC is the status (0 = OK, 1 = Buffer Full)
    */
 
   a = (long *)prec->a;
@@ -29,17 +31,21 @@ static long buffer(aSubRecord *prec) {
       vala[_index] = b[0];
       _index++;
       valb[0] = _index;
+      prec->neva = _index;
       valc[0] = 0;
     } else {
       valc[0] = 1;
     }
   } else if(a[0] == 2){
-    // Clear the array
-    _index = 0;
-    valb[0] = 0; 
-    valc[0] = 0;
-    for(i=0;i<prec->nova;i++){
-      vala[i] = 0;
+    if(_index){
+      // Clear the array
+      _index = 0;
+      valb[0] = 0; 
+      valc[0] = 0;
+      prec->neva = prec->nova;
+      for(i=0;i<prec->nova;i++){
+        vala[i] = 0;
+      }
     }
   }
 
