c65d6cadc29db6b6dd132063a02a67eb57c65a88
John Sinsheimer <jsinsheimer@bnl.gov>
2020-11-29 14:58:47 -0500

    adding old commits

--- iocBoot/ioctimestamp/st.cmd
+++ iocBoot/ioctimestamp/st.cmd
@@ -11,6 +11,8 @@ timestamp_registerRecordDeviceDriver(pdbbase)
 
 ## Load record instances
 dbLoadRecords("../../db/timestamp.db","Sys=XF:23ID1-ES,Dev={FCCD-Timestamp},Inp=XF:23ID1-TS{EVR:1}EvtECnt-I")
+dbLoadRecords("$(EPICS_BASE)/db/iocAdminSoft.db","IOC=XF:23ID1-ES{FCCD-Timestamp}")
 
 iocInit()
 
+dbl > /cf-update/xf23id1-ioc2.es-timestamp.dbl
---
+++ st.cmd
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd iocBoot/ioctimestamp && ./st.cmd
